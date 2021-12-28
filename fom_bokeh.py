d7f1c316cca45e4a9ed3e771c01f9bcfced58902
Lee Aspitarte <aspitarte@gmail.com>
2021-12-28 10:56:49 -0800

    make some plot improvements

--- fom_bokeh.py
+++ fom_bokeh.py
@@ -1,7 +1,7 @@
 
 #%%
 from bokeh.plotting import figure, show, output_notebook
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, LabelSet
 output_notebook()
 
 from es import devices
@@ -11,19 +11,26 @@ from es import devices
 from bokeh.models import ColumnDataSource
 source = ColumnDataSource(dict(
 name=[name for name in devices],
-sm = [type(devices[name].storage_medium).__name__ for name in devices],
-pcs=[type(devices[name].transformation).__name__ for name in devices],
+long_name=[devices[name].name for name in devices],
+sm = [str(devices[name].storage_medium) for name in devices],
+pcs=[str(devices[name].transformation) for name in devices],
 y = [devices[name].storage_medium.energy_density.to('kWh/kg').magnitude for name in devices],
 x = [devices[name].transformation.power_flux.to('W/m^2').magnitude for name in devices]
 ))
 
 p = figure(y_axis_type='log', x_axis_type='log')
 p.add_tools(HoverTool(tooltips=[
-    ('name', '@name'),
-   ('sm', '@sm'),
-   ('pcs', '@pcs')
+    ('name', '@long_name'),
+   ('Storage Medium', '@sm'),
+   ('Power Conversion System', '@pcs')
    ]))
-p.circle(source=source, x='x',y='y', size=20)
+p.circle(source=source, x='x',y='y', size=50)
+
+
+labels = LabelSet(x='x', y='y', text='name', text_align='center', text_color='black',
+              x_offset=0, y_offset=0, source=source, render_mode='canvas')
+
+p.add_layout(labels)
 
 show(p)
 
