de6f3d91236b41bbb401791f3de4933ff371dd7c
Lee Aspitarte <aspitarte@gmail.com>
2021-12-27 22:48:56 -0800

    continue adding devices and classes

--- es.py
+++ es.py
@@ -11,22 +11,27 @@ def const_to_pint(phys_const):
 F = const_to_pint(physical_constants['Faraday constant'])
 e = const_to_pint(physical_constants['elementary charge'])
 M_nuc = const_to_pint(physical_constants['neutron mass'])
-
+mu_0 = const_to_pint(physical_constants['vacuum mag. permeability'])
+g = Quantity(9.8, 'm/s**2')
 
 
 #%%
-@dataclass()
+@dataclass(kw_only=True)
 class StorageMedium:
-    rho_m: float
+    rho_m: float = None
+    name: str = ''
 
-@dataclass()
+@dataclass(kw_only=True)
 class Transformation:
-    pass
+    SAV: float = None
+    rho_m: float = None
+    name:str = ''
 
 @dataclass
 class Device:
     storage_medium: StorageMedium
     transformation: Transformation
+    name: str = ''
 
     def ragone_coords(self):
         return (
@@ -77,6 +82,14 @@ class ChemicalEnergy(StorageMedium):
         return F*self.deltaV*self.n_e*self.f_active/self.M_mol
 
 @dataclass
+class GravitationalEnergy(StorageMedium):
+    deltaH: float
+
+    @property
+    def energy_density(self):
+        return self.deltaH*g
+
+@dataclass
 class ECDiffusion(Transformation):
     deltaV: float
     c_b: float
@@ -101,6 +114,22 @@ class ThermoMechanical(Transformation):
     def power_flux(self):
         return self.deltaP*self.velocity
 
+@dataclass
+class Mechanical(Transformation):
+    B: float
+    velocity: float = Quantity(100, 'm/s')
+
+    @property
+    def power_flux(self):
+        return self.velocity*self.B**2/(2*mu_0)
+
+
+@dataclass
+class Inverter(Transformation):
+
+    @property
+    def power_flux(self):
+        return Quantity(1e11, 'W/m^2') #TODO: formula?
 
 te_500K = ThermalEnergy(
     rho_m = Quantity(1, 'kg/L'), 
@@ -109,7 +138,7 @@ te_500K = ThermalEnergy(
     )
 
 VTsteel = VirialLimited(
-    rho_m = Quantity(1, 'kg/L'), 
+    rho_m = Quantity(8, 'kg/L'), 
     sigma_a= Quantity(400, 'MPa')
     )
 
@@ -129,11 +158,12 @@ lib_elec = ChemicalEnergy(
     rho_m=Quantity(1, 'kg/L')
 )
 
+#TODO: calculate for whole aqueous solution. How to handle cost calculation later on?
 vanadium_soln = ChemicalEnergy(
     deltaV=Quantity(1, 'V'),
     n_e=1,
-    f_active=1,
-    M_mol = Quantity(50, 'g/mol'),
+    f_active=0.01, 
+    M_mol = Quantity(18, 'g/mol'),
     rho_m=Quantity(1, 'kg/L')
 )
 
@@ -163,25 +193,57 @@ gt = ThermoMechanical(
     velocity = Quantity(100, 'm/s')
     )
 
+motor = Mechanical(B = Quantity(1,'T'))
+
 devices = {
     'ptes' : Device(
+        name='Pumped Thermal Energy Storage',
         storage_medium=te_500K,
         transformation=gt
     ),
     'rfc': Device(
+        name='Fuel Cell/Electrolysis',
         storage_medium=hydrocarbon,
         transformation=diff_gdl
     ),
     'conv_power': Device(
+        name='Combined Cycle Power Generation',
         storage_medium=hydrocarbon,
         transformation=gt
     ),
     'caes': Device(
+        name='Diabatic/Isothermal Compressed Air',
         storage_medium=VTsteel,
         transformation=gt
     ),
     'vrfb': Device(
+        name='Vanadium Redox Flow Battery',
         storage_medium=vanadium_soln,
         transformation=diff_aq
+    ),
+    'flywheel': Device(
+        name='Flywheel Energy Storage',
+        storage_medium=VTsteel,
+        transformation=motor
+    ),
+    'phes': Device(
+        name='Pumped Hydro',
+        storage_medium=GravitationalEnergy(
+            rho_m=Quantity(1, 'kg/L'),
+            deltaH=Quantity(100,'m')
+        ),
+        transformation=motor
+    ),
+    'lib': Device(
+        name='Lithium Ion Battery',
+        storage_medium=lib_elec,
+        transformation=diff_li
+    ),
+    'smes': Device(
+        name='Superconducting Magnetic',
+        storage_medium=VTsteel,
+        transformation= Inverter()
     )
 }
+
+# %%
