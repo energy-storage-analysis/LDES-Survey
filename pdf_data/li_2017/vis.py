ceb2c52c7105215dce655550e61481c936733e04
Lee Aspitarte <aspitarte@gmail.com>
2022-02-14 09:20:17 -0800

    improve plots

--- pdf_data/li_2017/vis.py
+++ pdf_data/li_2017/vis.py
@@ -14,12 +14,12 @@ df.info()
 df['type'].value_counts().plot(kind='bar')
 #%%
 
-df_phys = df[['type','cost','year']]
+df_phys = df[['type','C_kwh','year']]
 df_phys
 #%%
 import seaborn as sns
 
-sns.boxplot(x='type',y='cost',data=df_phys)
+sns.boxplot(x='type',y='C_kwh',data=df_phys)
 plt.xticks(rotation=90)
 plt.yscale('log')
 #%%
@@ -27,14 +27,14 @@ plt.yscale('log')
 df_plot = df_phys#.where(df_phys['class'] == 'Glycols')
 
 plt.figure(figsize = (15,6))
-sns.swarmplot(x='type',y='cost',data=df_plot)
+sns.swarmplot(x='type',y='C_kwh',data=df_plot)
 plt.xticks(rotation=90)
 
 plt.yscale('log')
 #%%
 
 plt.figure(figsize = (15,6))
-sns.violinplot(x='type',y='cost',data=df_phys)
+sns.violinplot(x='type',y='C_kwh',data=df_phys)
 plt.xticks(rotation=90)
 # plt.yscale('log')
 # plt.ylim(1e1,)
@@ -43,26 +43,27 @@ plt.xticks(rotation=90)
 # %%
 import numpy as np
 bins = np.logspace(np.log10(8e1), np.log10(1e3), 30)
-df['cost'].hist(bins=bins)
+df['C_kwh'].hist(bins=bins)
 plt.xscale('log')
-plt.xlabel('cost')
+plt.xlabel('C_kwh')
 #%%
 from pandas.plotting import scatter_matrix
 scatter_matrix(df, figsize=(10,10))
 #%%
 
-cols = ['type','year','cost']
+cols = ['type','year','C_kwh']
 df_sel = df.dropna(subset=cols, how='any')[cols]
-df_sel = df_sel.sort_values('cost')
-g = sns.scatterplot(data=df_sel, x='cost', y='type', hue='year')
-plt.xscale('log')
+df_sel = df_sel.sort_values('C_kwh')
+g = sns.scatterplot(data=df_sel, y='C_kwh', x='year', hue='type')
+plt.yscale('log')
+plt.xlim(1950,)
 
 plt.gca().get_legend().set_bbox_to_anchor([0,0,1.5,1])
 
 #plt.locator_params(axis='x', nbins=5)
 #g.set_xticklabels(rotation=45)
-#s = df_sel.set_index(['class','cost'])['sp_latent_heat']
-#s.groupby('class').plot(x='cost')
+#s = df_sel.set_index(['class','C_kwh'])['sp_latent_heat']
+#s.groupby('class').plot(x='C_kwh')
 #plt.legend()
 
 #%%
@@ -71,10 +72,10 @@ df_table3 = pd.read_csv('output/table_3.csv', index_col=0)
 df_table3
 #%%
 
-df_table3['cost'].hist()
+df_table3['C_kwh'].hist()
 #%%
 
 plt.figure(figsize = (15,6))
-sns.violinplot(y='cost',data=df_phys)
+sns.violinplot(y='C_kwh',data=df_phys)
 plt.xticks(rotation=90)
 # %%
--- pdf_data/vis_together.py
+++ pdf_data/vis_together.py
@@ -10,6 +10,10 @@ sys.path.append('..')
 df_latent = pd.read_csv('alva/output/table_8.csv', index_col=0)
 
 df_latent['C_kwh'] = df_latent['cost']/(df_latent['sp_latent_heat'])
+df_latent['energy_type'] = 'Latent Thermal'
+df_latent['source'] = 'Alva et al. 2018'
+
+df_latent = df_latent.rename({'material':'name'}, axis=1)
 
 df_4 = pd.read_csv('alva/output/table_4.csv', index_col=0)
 df_5 = pd.read_csv('alva/output/table_5.csv', index_col=0)
@@ -18,36 +22,30 @@ df_7 = pd.read_csv('alva/output/table_7.csv', index_col=0)
 
 col_sel = ['Cp', 'kth', 'cost', 'class']
 
-
 df_sens = pd.concat([df[col_sel] for df in [df_4, df_5, df_6, df_7]])
-
-df_sens['Cp'] = df_sens['Cp'].astype(float)
+df_sens = df_sens.reset_index() #name was the index
 df_sens['Cp'] = df_sens['Cp']/3600
-
-df_sens['kth'] = df_sens['kth'].astype(float)
-
-# df_sens['cost'] = df_sens['cost'].str.replace(',','.')
-df_sens['cost'] = df_sens['cost'].astype(float)
-
 df_sens['C_kwh'] = df_sens['cost']/(df_sens['Cp']*500)
 
-df_sens
+df_sens['energy_type'] = 'Sensible Thermal'
+df_sens['source'] = 'Alva et al. 2018'
 
 
 #%%
 df_ec = pd.read_csv('li_2017/output/table_2.csv', index_col=0)
-df_ec
+df_ec = df_ec.rename({'label':'name'},axis=1)
 
+df_ec['energy_type'] = 'Chemical (electrochemical)'
+df_ec['source'] = 'Li et al. 2017'
 
 #%%
 
-df_virial = pd.read_csv('nomura_2017/table_2_mat.csv', index_col=0)
+# df_virial = pd.read_csv('nomura_2017/table_2_mat.csv', index_col=0)
 
-df_virial['specific_energy'] = (1*df_virial['max stress'])/df_virial['density']
-df_virial['specific_energy'] = df_virial['specific_energy']/3600000
-df_virial['C_kwh'] = df_virial['mat_cost']/df_virial['specific_energy']
+# df_virial['specific_energy'] = (1*df_virial['max stress'])/df_virial['density']
+# df_virial['specific_energy'] = df_virial['specific_energy']/3600000
+# df_virial['C_kwh'] = df_virial['mat_cost']/df_virial['specific_energy']
 
-df_virial
 
 #%%
 
@@ -55,39 +53,71 @@ from kale_2018.vis import load_tables
 
 kale_tables = load_tables('kale_2018')
 
-# %%
+kale_tables['a1']['energy_type'] = 'Virial (Metal)'
+kale_tables['a1']['source'] = 'Kale 2018'
+kale_tables['a1'] = kale_tables['a1'].rename({'Material': 'name'}, axis=1)
 
-s_latent = df_latent['C_kwh'].dropna()
-s_sens = df_sens['C_kwh'].dropna()
-s_ec = df_ec['C_kwh'].dropna()
-s_virial_metal = kale_tables['a1']['C_kwh'].dropna()
-s_virial_comp = kale_tables['a2']['C_kwh'].dropna()
+kale_tables['a2']['energy_type'] = 'Virial (Composite)'
+kale_tables['a2']['source'] = 'Kale 2018'
+kale_tables['a2'] = kale_tables['a2'].rename({'Material': 'name'}, axis=1)
 
-dataset = [
-    s_latent.values,
-    s_sens.values,
-    s_ec.values,
-    s_virial_metal.values,
-    s_virial_comp.values
-]
+#%%
 
-labels=[
-    'Latent Thermal \n(Alva et al 2018)', 
-    'Sensible Thermal (deltaT = 500K)\n(Alva et al 2018)',
-    'Chemical (electrochemical)\n(Li et al 2017)',
-    'Viral (metals)\n(Kale et al. 2018)', 
-    'Viral (composites)\n(Kale et al. 2018)'
-    ]
+cols = ['C_kwh','energy_type','source','name']
 
-plt.figure(figsize = (15,6))
-plt.violinplot(dataset=dataset, positions=range(len(dataset)))
-sns.stripplot(data=dataset)
+df_all = pd.concat([
+df_latent[cols].dropna(),
+df_sens[cols].dropna(),
+df_ec[cols].dropna(),
+kale_tables['a1'][cols].dropna(),
+kale_tables['a2'][cols].dropna()
+])
 
-plt.axhline(10, linestyle='--', color='gray')
+df_all
+#%%
+from matplotlib import ticker as mticker
 
-plt.gca().set_xticks(np.arange(0, len(labels)), labels=labels)
+df_all['C_kwh_log'] = np.log10(df_all['C_kwh'])
+df_all['cat_label'] = df_all['energy_type'] + '\n(' + df_all['source'] + ')'
 
-plt.yscale('log')
+fig = plt.figure(figsize = (15,6))
+# plt.violinplot(dataset=df_all['C_kwh'].values)
+sns.violinplot(data=df_all, x='cat_label', y='C_kwh_log')
+# sns.stripplot(data=df_all_plot, x='energy_type', y='C_kwh')
+
+# plt.axhline(1, linestyle='--', color='gray')
+
+fig.axes[0].yaxis.set_major_formatter(mticker.StrMethodFormatter("$10^{{{x:.0f}}}$"))
+fig.axes[0].yaxis.set_ticks([np.log10(x) for p in range(-2,5) for x in np.linspace(10**p, 10**(p+1), 10)], minor=True)
+# plt.gca().set_xticks(np.arange(0, len(labels)), labels=labels)
+
+# plt.yscale('log')
 # plt.xticks(rotation=90)
 plt.ylabel('Material Energy Cost ($/kWh)')
 # %%
+import iqplot
+from bokeh.io import output_notebook, show
+from bokeh.models import ColumnDataSource, HoverTool, Range1d
+from bokeh.io import output_file
+
+# output_notebook()
+
+# source = ColumnDataSource(df_all)
+
+figure = iqplot.strip(
+    data=df_all, cats='cat_label', q='C_kwh', 
+    q_axis='y',y_axis_type='log' ,
+    jitter=True,
+    tooltips=[('name','@name')],
+    plot_width = 1000,plot_height=700,
+    marker_kwargs={'size':10}
+    )
+
+figure.xaxis.major_label_orientation = np.pi/4
+figure.yaxis.axis_label = "Energy Capital Cost ($/kWh)"
+# show(figure)
+figure.yaxis.axis_label_text_font_size = "16pt"
+figure.xaxis.major_label_text_font_size = "16pt"
+
+output_file('mat_cost_compare.html')
+show(figure)
