ac7c6384d5184237d05ce35540ea32b3525ebfbc
Lee Aspitarte <aspitarte@gmail.com>
2022-02-22 18:08:19 -0800

    remove slider from duration
    appears that bokeh can only have 9 sliders on one page?

--- lcos/lcos_duration.py
+++ lcos/lcos_duration.py
@@ -22,16 +22,16 @@ Ckwh_slider = Slider(start=1, end=300, value=100, step=1, title="Energy Capital 
 Ckw_slider = Slider(start=10, end=10000, value=100, step=10, title="Power Capital Cost ($/kWh)")
 eta_slider = Slider(start=0.1, end=1, value=0.8, step=.1, title="Round Trip Efficiency")
 lifetime_slider = Slider(start=5, end=50, value=20, step=5, title="Lifetime (Years)")
-PE_slider = Slider(start=1, end=20, value=2.5, step=0.5, title="Price Electricity (cents/kWh)")
+# PE_slider = Slider(start=1, end=20, value=2.5, step=0.5, title="Price Electricity (cents/kWh)")
 
-callback = CustomJS(args=dict(source=source, Ckwh_slider=Ckwh_slider, Ckw_slider=Ckw_slider, eta_slider=eta_slider, lifetime_slider=lifetime_slider, PE_slider=PE_slider),
+callback = CustomJS(args=dict(source=source, Ckwh_slider=Ckwh_slider, Ckw_slider=Ckw_slider, eta_slider=eta_slider, lifetime_slider=lifetime_slider),
                     code="""
     const data = source.data;
     const Ckwh = Ckwh_slider.value;
     const Ckw = Ckw_slider.value;
     const eta = eta_slider.value;
     const lifetime = lifetime_slider.value;
-    const PE = PE_slider.value/100;
+    const PE = 0.025;
     const duration = data['duration']
     const lcos = data['lcos']
     for (let i = 0; i < duration.length; i++) {
@@ -44,14 +44,14 @@ Ckwh_slider.js_on_change('value', callback)
 Ckw_slider.js_on_change('value', callback)
 eta_slider.js_on_change('value', callback)
 lifetime_slider.js_on_change('value', callback)
-PE_slider.js_on_change('value', callback)
+# PE_slider.js_on_change('value', callback)
 
 # amp_slider.trigger('value', 1, 1.1)
 # callback._trigger_event()
 
 layout = row(
     plot,
-    column(Ckwh_slider, Ckw_slider, eta_slider, lifetime_slider, PE_slider),
+    column(Ckwh_slider, Ckw_slider, eta_slider, lifetime_slider),
 )
 
 
