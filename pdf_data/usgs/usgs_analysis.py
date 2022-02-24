bd3df58dd019ff70da5199bc2f119b69a998ce4b
Lee Aspitarte <aspitarte@gmail.com>
2022-02-24 12:15:43 -0800

    add commodity analysis to usgs

--- pdf_data/usgs/usgs_analysis.py
+++ pdf_data/usgs/usgs_analysis.py
@@ -61,15 +61,39 @@ df_low.sort_values('price_per_kg').iloc[0:50]
 # %%
 avg_price = df.groupby('Commodity')['price_per_kg'].mean()
 avg_price = avg_price.sort_values()
-avg_price
-
-#%%
+avg_price.name = 'average_price'
 avg_price.iloc[0:50]
+
 # %%
 bins = np.logspace(np.log10(0.1), np.log10(1e6), 50)
 avg_price.hist(bins=bins)
 plt.xscale('log')
 plt.xlabel('Cost ($/kg)')
 plt.ylabel('Count')
+
+#%%
+std_price = df.groupby('Commodity')['price_per_kg'].std()
+std_price = std_price.sort_values()
+std_price.name = 'stddev_price'
+# %%
+commodity_counts = df['Commodity'].sort_values(ascending=False).value_counts()
+commodity_counts.name = 'num_entries'
+
+df_commodity = pd.concat([
+    commodity_counts,
+    avg_price,
+    std_price 
+], axis=1)
+df_commodity
+#%%
+
+df_commodity['ratio'] = df_commodity['stddev_price']/df_commodity['average_price']
+
+df_commodity.sort_values('ratio', ascending=False)
+#%%
+
+
+df_commodity['formula'] = ''
+df_commodity.to_csv('data/commodity.csv')
 
 # %%
