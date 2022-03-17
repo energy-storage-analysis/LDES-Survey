77efbf8bea0d66d705e607838fca9a3aa25d2b7e
Lee Aspitarte <aspitarte@gmail.com>
2022-03-16 23:28:30 -0700

    add basic bokeh C_kwh plot

---
+++ mat_cost/bokeh_Ckwh.py
@@ -0,0 +1,44 @@
+#%%``
+import pandas as pd
+from bokeh.plotting import figure, show, save, output_file
+from bokeh.transform import factor_cmap, factor_mark
+from bokeh.models import HoverTool
+
+#%%
+df = pd.read_csv('data/C_kwh.csv')
+df = df.where(df['energy_type'] != 'Electrostatic (Capacitor)').dropna(subset=['energy_type'])
+
+#%%
+
+energy_types = df['energy_type'].unique()
+energy_types
+
+#https://docs.bokeh.org/en/latest/docs/user_guide/plotting.html#userguide-plotting-scatter-markers
+MARKERS = ['circle','square','triangle','star','plus','inverted_triangle','hex','diamond','cross','square_x']
+MARKERS = MARKERS[0:len(energy_types)]
+
+color_category = 'Category10_{}'.format(len(energy_types))
+
+#%%
+p = figure(background_fill_color="#fafafa", y_axis_type='log',x_axis_type='log',plot_width=1500,plot_height=800)
+
+p.xaxis.axis_label = 'Energy Density (kWh/kg)'
+p.yaxis.axis_label = 'Material Cost ($/kg)'
+
+p.scatter("specific_energy", "specific_price", source=df,
+          legend_group="energy_type", fill_alpha=0.5, size=20,
+          marker=factor_mark('energy_type', MARKERS, energy_types),
+          color=factor_cmap('energy_type', color_category, energy_types))
+
+p.legend.location = "top_left"
+p.legend.title = "energy_type"
+
+hovertool = HoverTool(tooltips=[('index','@index'), ('source','@source')])
+p.add_tools(hovertool)
+
+
+output_file('output/bokeh_Ckwh.html')
+save(p)
+
+
+# %%
