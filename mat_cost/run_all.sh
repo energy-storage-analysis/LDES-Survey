1270d75a8244ed9c30923cae5cb98abb31b0e9c3
Lee Aspitarte <aspitarte@gmail.com>
2022-03-18 11:32:01 -0700

    add echo output to shell script

--- mat_cost/run_all.sh
+++ mat_cost/run_all.sh
@@ -1,6 +1,17 @@
+echo "Consolidating Data"
 python "consolidate_data.py"
+
+
+echo "Calculating C_kWh"
 python "calc_Ckwh.py"
+
+
+echo "Generate Missing Data Info"
+python "missing_data.py"
+
+echo "Generate visualizations"
 python "Ckwh_vis.py"
 python "separate_Ckwh_vis.py"
 python "bokeh_Ckwh.py"
-python "missing_data.py"
+
+echo "Done"
