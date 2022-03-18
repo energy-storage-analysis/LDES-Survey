c530f430a9c3a9546fc8cefad21b76c0488c2313
Lee Aspitarte <aspitarte@gmail.com>
2022-03-17 17:32:29 -0700

    specify physprop and price sources

--- mat_cost/bokeh_Ckwh.py
+++ mat_cost/bokeh_Ckwh.py
@@ -47,7 +47,7 @@ p.line(energy_densities_line, mat_cost_line)
 p.legend.location = "top_left"
 p.legend.title = "energy_type"
 
-hovertool = HoverTool(tooltips=[('index','@index'), ('source','@source')])
+hovertool = HoverTool(tooltips=[('index','@index'), ('physical property source','@physprop_source'), ('price_sources', '@price_sources')])
 p.add_tools(hovertool)
 
 
--- mat_cost/calc_Ckwh.py
+++ mat_cost/calc_Ckwh.py
@@ -75,28 +75,32 @@ df_out = pd.concat([
     gravitational
 ]).dropna()
 
+df_out = df_out.rename({'source': 'physprop_source'}, axis=1)
+
 df_out
 
 #%%
 prices = [df_prices['specific_price'][p] if p in df_prices.index else np.nan for p in df_out.index]
+sources = [df_prices['source'][p] if p in df_prices.index else np.nan for p in df_out.index]
 
 df_out['specific_price'] = prices
+df_out['price_sources'] = sources
 
 
-#drop any whwere price or energy data is missing
-#TODO: make aware of missing prices and materials 
-df_out = df_out.dropna(how='any')
+# #drop any whwere price or energy data is missing
+# #TODO: make aware of missing prices and materials 
+# df_out = df_out.dropna(subset=['specific_price', 'specific_energy'], how='any')
 
 df_out
 
 #%%
 df_ec_li = pd.read_csv(r'C:\Users\aspit\Git\MHDLab-Projects\Energy-Storage-Analysis\datasets\pub\li_2017\output\couples.csv',index_col=0)
-df_ec_li['source'] = 'Li 2017'
+df_ec_li['physprop_source'] = 'Li 2017'
 df_ec_lmb = pd.read_csv(r'C:\Users\aspit\Git\MHDLab-Projects\Energy-Storage-Analysis\datasets\pub\kim_2013\output\couples.csv', index_col=0)
 df_ec_lmb['type'] = 'Liquid Metal'
-df_ec_lmb['source'] = 'Kim 2013'
+df_ec_lmb['physprop_source'] = 'Kim 2013'
 
-col_select = ['type','A','B','mu_A', 'mu_B', 'deltaV', 'specific_energy', 'source']
+col_select = ['type','A','B','mu_A', 'mu_B', 'deltaV', 'specific_energy', 'physprop_source']
 
 df_ec = pd.concat([
     df_ec_li[col_select],
@@ -104,7 +108,11 @@ df_ec = pd.concat([
 ])
 
 df_ec['SP_A'] = [df_prices['specific_price'][f] if f in df_prices.index else np.nan for f in df_ec['A']]
+df_ec['price_source_A'] = [df_prices['source'][f] if f in df_prices.index else np.nan for f in df_ec['A']]
 df_ec['SP_B'] = [df_prices['specific_price'][f] if f in df_prices.index else np.nan for f in df_ec['B']]
+df_ec['price_source_B'] = [df_prices['source'][f] if f in df_prices.index else np.nan for f in df_ec['B']]
+
+df_ec['price_sources'] = "A: " + df_ec['price_source_A'] + " --- B: " + df_ec['price_source_B']
 
 #TODO: chech this equation
 df_ec['specific_price'] = (df_ec['SP_A']*df_ec['mu_A'] + df_ec['SP_B']*df_ec['mu_B'])/(df_ec['mu_A']+df_ec['mu_B'])
@@ -120,11 +128,13 @@ df_ec.to_csv('data/df_couples.csv')
 
 
 df_out = pd.concat([
-    df_ec[['specific_energy', 'energy_type','specific_price','source']],
+    df_ec[['specific_energy', 'energy_type','specific_price','physprop_source', 'price_sources']],
     df_out,
-]).dropna()
+])
 
 df_out['C_kwh'] = df_out['specific_price']/df_out['specific_energy']
+
+df_out = df_out.dropna(subset=['C_kwh'])
 
 # %%
 
