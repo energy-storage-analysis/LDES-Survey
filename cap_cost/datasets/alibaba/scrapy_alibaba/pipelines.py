e9df6a16520079c24ae80ffa0fa9d34b04c4afb7
Lee Aspitarte <aspitarte@gmail.com>
2022-04-29 11:43:41 -0700

    improve pipeline adding jl for dropped data

--- cap_cost/datasets/alibaba/scrapy_alibaba/pipelines.py
+++ cap_cost/datasets/alibaba/scrapy_alibaba/pipelines.py
@@ -12,15 +12,35 @@ class ScrapyAlibabaPipeline(object):
 
     def open_spider(self, spider):
         self.file = open('items.jl', 'w')
+        self.dropfile = open('items_dropped.jl', 'w')
 
     def close_spider(self, spider):
         self.file.close()
+        self.dropfile.close()
 
     def process_item(self, item, spider):
-        if item['must_contain'].lower() in item['title'].lower():
-            line = json.dumps(ItemAdapter(item).asdict()) + "\n"
-            self.file.write(line)
-            return item
-        else:
-            raise DropItem("Title Did not contain {}".format(item['must_contain']))
+        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+
+        if not item['price']: 
+            self.dropfile.write(line)
+            raise DropItem("Missing Price")
+
+        if item['must_contain'] == item['must_contain']:
+            must_contain_strs = [s.strip().lower() for s in item['must_contain'].split(',')]
+            contain_conditions = [s in item['title'].lower() for s in must_contain_strs]
+            if not any(contain_conditions):
+                self.dropfile.write(line)
+                raise DropItem("Title Did not contain {}".format(item['must_contain']))
+
+        if not item['min_order']:
+            self.dropfile.write(line)
+            raise DropItem("Missing Minimum Order")
+
+        if 'piece' in item['min_order'].lower():
+            self.dropfile.write(line)
+            raise DropItem("'piece' found in Minimum Order ")
+
+        #All conditions met, write to file
+        self.file.write(line)
+        return item
 
