7ea3d9e1e24983c3ebf867f9699290c499574315
Lee Aspitarte <aspitarte@gmail.com>
2022-06-29 00:31:34 -0700

    add visuals positional arg to run_all

--- cap_cost/run_all.sh
+++ cap_cost/run_all.sh
@@ -4,8 +4,12 @@ python "consolidate_data.py"
 echo "Calculating C_kWh"
 python "calc_Ckwh.py"
 
-echo "Generate visualizations"
-cd analysis
-./genvis_all.sh
+
+
+if [ "$1" == "vis" ]; then
+    echo "Generate visualizations"
+    cd analysis
+    ./genvis_all.sh
+fi
 
 echo "Done"
