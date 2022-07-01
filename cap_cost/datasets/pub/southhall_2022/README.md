2fa4a61b55a10d9b987c933954721674b5871159
Lee Aspitarte <aspitarte@gmail.com>
2022-06-30 19:41:25 -0700

    have LOHC use table H2 weight percents

--- cap_cost/datasets/pub/southhall_2022/README.md
+++ cap_cost/datasets/pub/southhall_2022/README.md
@@ -1,5 +1,11 @@
 Southall, E., and Lukashuk, L. “Analysis of Liquid Organic Hydrogen Carrier Systems.” Johnson Matthey Technology Review, 2022. https://doi.org/10.1595/205651322X16415722152530.
 
-The data from table 1 was pulled manually to form material prices. The number of hydrogen molecules released under dehydrogenation is used to calculate the total molar Gibbs free energy of the liberated hydrogen gas, which is used along with the weight of the organic molecule to determine the specific energy.
+The data from table 1 was pulled manually to form material prices. The weight percent of hydrogen ($ W_{H_2} $) is used to calculate the overall Gibbs free energy per  mole of host molecule. 
+
+$$ \Delta G [\frac{kWh}{mol_{host}}] = \Delta G[\frac{kWh}{mol_{H_2}}] \times r_{H_2} $$
+
+$$ r_{H_2} = \frac{W_{H_2} \mu_{host}}{(1-W_{H_2}) \mu_{H_2}}$$
+
+Molecular formulas taken from Modisha 2019
 
 TODO: Currently the enthalpy of dehydrogenation is not taken into account and probably should be subtracted from the hydrogen gas Gibbs energy. This could be handled similarly to Hurskainen 2020 Table 3. 
--- cap_cost/datasets/pub/southhall_2022/process.py
+++ cap_cost/datasets/pub/southhall_2022/process.py
@@ -5,26 +5,46 @@ from sympy import O
 from es_utils.chem import get_molecular_mass
 from es_utils.units import prep_df_pint_out
 
-df = pd.read_csv('input.csv', index_col=0)
-df['SM_type'] = 'synfuel'
-df['sub_type'] = 'LOHC'
+df_SM = pd.read_csv('input.csv', index_col=0)
+df_SM['SM_type'] = 'synfuel'
+df_SM['sub_type'] = 'LOHC'
 
-df
-# %%
+df_SM['wt_pct'] = df_SM['wt_pct']/100
+
+df_SM['mu_host'] = df_SM['MF_host'].apply(get_molecular_mass)
+
+mu_H = 1.0078
+
+
+df_SM['r_H2'] = (df_SM['wt_pct']*df_SM['mu_host'])/((1-df_SM['wt_pct'])*mu_H )/2
 
 deltaG_H2 = 0.0659 #kWh/molH2
 
-total_deltaG = deltaG_H2*df['N_H2']
+df_SM['deltaG_chem'] = df_SM['r_H2'] * deltaG_H2
+df_SM['n_e'] = df_SM['r_H2']*2
 
-df['deltaG_chem'] = total_deltaG
-df['n_e'] = 2*df['N_H2']
+# df_SM = df_SM[['original_name','SM_type','sub_type','materials','mat_basis','deltaG_chem','n_e']]
+
+df_SM['deltaG_chem'] = df_SM['deltaG_chem'].astype('pint[kWh/mol]')
+df_SM['n_e'] = df_SM['n_e'].astype('pint[dimensionless]')
+
+df_SM
 
 
 
-df
+# deltaG_H2 = 0.0659 #kWh/molH2
+
+# total_deltaG = deltaG_H2*df['N_H2']
+
+# df['deltaG_chem'] = total_deltaG
+# df['n_e'] = 2*df['N_H2']
+
+
+
+df_SM
 # %%
 
-df_SM = df[['materials','mat_basis','SM_type','sub_type','deltaG_chem','n_e']]
+df_SM = df_SM[['materials','mat_basis','SM_type','sub_type','deltaG_chem','n_e']]
 
 
 df_SM['deltaG_chem'] = df_SM['deltaG_chem'].astype('pint[kWh/mol]')
