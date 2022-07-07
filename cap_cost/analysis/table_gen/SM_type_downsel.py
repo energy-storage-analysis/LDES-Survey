7ea94c930509bd91c7b3b833364a2bb4adf4b866
Lee Aspitarte <aspitarte@gmail.com>
2022-07-07 15:29:39 -0700

    make csv file for each SM type

--- cap_cost/analysis/table_gen/SM_type_downsel.py
+++ cap_cost/analysis/table_gen/SM_type_downsel.py
@@ -3,9 +3,9 @@
 #%%
 import os
 from os.path import join as pjoin
-from es_utils.units import read_pint_df
+from es_utils.units import prep_df_pint_out, read_pint_df
 
-output_dir = 'output'
+output_dir = 'output/individual'
 if not os.path.exists(output_dir): os.makedirs(output_dir)
 
 from dotenv import load_dotenv
@@ -13,18 +13,15 @@ load_dotenv()
 REPO_DIR = os.getenv('REPO_DIR')
 
 df_SMs = read_pint_df(pjoin(REPO_DIR, 'cap_cost/data_consolidated/SM_data.csv'), index_col=[0,1])
+df_SMs = df_SMs.reset_index('SM_type')
 
 #%%
 
-df = df_SMs.reset_index('SM_type')
+for SM_type in df_SMs['SM_type'].values:
+    df_sel = df_SMs[df_SMs['SM_type'].isin([SM_type])].dropna(subset=['SM_type'])
 
+    df_sel = df_sel.dropna(axis=1, how='all')
 
-df = df[df['SM_type'].isin([
-    'flow_battery',
-    'hybrid_flow'
-])].dropna(subset=['SM_type'])
+    df_sel = prep_df_pint_out(df_sel)
 
-
-df = df.dropna(axis=1, how='all')
-
-df.to_csv(os.path.join(output_dir,'SM_type_downsel.csv'))
+    df_sel.to_csv(os.path.join(output_dir,'{}.csv'.format(SM_type)))
