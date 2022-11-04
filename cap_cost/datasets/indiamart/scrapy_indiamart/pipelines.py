54979574b817e0842d6c768164ef879a86c9df42
Lee Aspitarte <aspitarte@gmail.com>
2022-11-04 15:01:02 -0700

    add initial indiamart scraper

---
+++ cap_cost/datasets/indiamart/scrapy_indiamart/pipelines.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import os
+from scrapy.exceptions import DropItem
+from itemadapter import ItemAdapter
+
+class ScrapyIndiamartPipeline(object):
+
+    def open_spider(self, spider):
+
+        if not os.path.exists('output'): os.mkdir('output')
+
+        #with the -a spider_iter=<num> kwarg the file can instead be output to output/temp to be combined later. 
+        if hasattr(spider, 'spider_iter'):
+            i = spider.spider_iter
+            self.file = open('output/temp/items_{}.jl'.format(i), 'w')
+            self.dropfile = open('output/temp/items_dropped_{}.jl'.format(i), 'w')
+        else:
+            self.file = open('output/items.jl', 'w')
+            self.dropfile = open('output/items_dropped.jl', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+        self.dropfile.close()
+
+    def drop_item(self, item, message):
+        item['drop_reason'] = message
+        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        self.dropfile.write(line)
+        raise DropItem(message)
+
+    def process_item(self, item, spider):
+        
+        # #TODO: figure out price_normal and price_promotion (see css selctors). I think this will be fixed if we just follow the response to the product page, vs trying to pull the price from the search results.
+        # if not item['price_normal'] and not item['price_promotion']: 
+        #     self.drop_item(item, "Missing Price")
+
+        if item['must_contain'] == item['must_contain']:
+            must_contain_strs = [s.strip().lower() for s in item['must_contain'].split(',')]
+            contain_conditions = [s in item['title'].lower() for s in must_contain_strs]
+            if not any(contain_conditions):
+                self.drop_item(item,"Title Did not contain {}".format(item['must_contain']))
+
+        # if not item['min_order']:
+        #     self.drop_item(item, "Missing Minimum Order")
+
+        # if 'piece' in item['min_order'].lower():
+        #     self.drop_item(item,"'piece' found in Minimum Order " )
+
+        #All conditions met, write to file
+        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
+        self.file.write(line)
+        return item
+
---
+++ cap_cost/datasets/indiamart/scrapy_indiamart/spiders/indiamart_crawler.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import Request
+import csv
+import os
+from selectorlib import Extractor
+import re 
+import itertools
+import sys
+import pandas as pd
+
+import urllib.parse
+
+START_ROW = 0
+# NUM_ROWS = sys.maxsize
+NUM_ROWS = 3
+
+RESOURCE_FOLDER = os.path.join(os.path.dirname(__file__), "../resources")
+
+def format_url(search_str):
+    search_str = search_str.replace(" ", "+")
+    search_str = urllib.parse.quote(search_str, safe="+")
+    url = "https://dir.indiamart.com/search.mp?ss={}&mcatid=12450&catid=180&prdsrc=1&stype=attr=1".format(search_str)
+    return url
+
+class IndiamartCrawlerSpider(scrapy.Spider):
+    name = 'indiamart_crawler'
+    allowed_domains = ['indiamart.com']
+    start_urls = ['http://indiamart.com/']
+    extractor = Extractor.from_yaml_file(os.path.join(RESOURCE_FOLDER, "search_results.yml"))
+
+    def start_requests(self):
+        """Read keywords from keywords file amd construct the search URL"""
+
+        df_search = pd.read_csv(os.path.join(RESOURCE_FOLDER, 'mat_data_searches.csv'), index_col=0)
+        df_search = df_search.iloc[START_ROW:START_ROW + NUM_ROWS]
+        df_search = df_search[['search_text', 'must_contain', 'split_hydrate']]
+        df_search = df_search.dropna(subset=['search_text'])
+
+        for idx, row in df_search.iterrows():
+            search_text=row["search_text"]
+
+            url = format_url(search_text)
+            # The meta is used to send our search text into the parser as metadata
+            meta_dict = {
+            'index': idx,
+            'search_text': search_text,
+            'must_contain': row['must_contain'],
+            'split_hydrate': row['split_hydrate']
+            }
+
+            yield scrapy.Request(url, callback = self.parse, meta = meta_dict, cb_kwargs=meta_dict)
+
+
+    def parse(self, response, **meta_dict):
+        data = self.extractor.extract(response.text,base_url=response.url)
+        if data['products']:
+            for product in data['products']:
+                outdict = {}
+                outdict.update(meta_dict)
+                outdict.update(product)
+                yield outdict
