ce53c6ae21174cca2e0fd9fcc0b2a32e6b6646a4
Lee Aspitarte <aspitarte@gmail.com>
2023-02-03 13:03:07 -0800

    add figure generation main sh script

--- run_all.sh
+++ run_all.sh
@@ -3,4 +3,26 @@
 
 # Export the vars in .env into your shell:
 # https://gist.github.com/judy2k/7656bfe3b322d669ef75364a46327836
-export $(egrep -v '^#' ../.env | xargs)
+# export $(egrep -v '^#' ../.env | xargs)
+
+# Generate visualizations
+
+cd lcos
+echo "Generating LCOS Figure Panels"
+python vis_lcos_pub.py
+cd ..
+
+cd GESDB
+echo "Generating GESDB Figure Panels"
+python gesdb_analysis.py
+cd ..
+
+cd cap_cost
+echo "Generating database and figure panels"
+./run_all.sh vis
+cd ..
+
+cd figures
+echo "Generating final figures from svg files"
+python genfigs.py
+cd ..
