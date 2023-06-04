ad5fb4635efdcaee645cd3bbcd241f834a8dd6b8
Lee Aspitarte <aspitarte@gmail.com>
2023-06-03 21:01:12 -0700

    separate out vol priced components

--- cap_cost/figure_panels/Ckwh_strip/Ckwh.py
+++ cap_cost/figure_panels/Ckwh_strip/Ckwh.py
@@ -48,28 +48,47 @@ median_Ckwh = df_all.groupby('SM_type')['C_kwh'].median().to_dict()
 df_all['Ckwh_SMtype_median'] = df_all['SM_type'].map(median_Ckwh)
 df_all = df_all.sort_values('Ckwh_SMtype_median')#.sort_values('energy_type')
 
+# df_all
 
 #%%
 
+# Separate out volumetric 
 
-def calc_DDmax(log_C_kWh):
-    return 2190/(10**log_C_kWh)
+syn_tanks = df_all.where(df_all['SM_type'] == 'synfuel').where(df_all['sub_type'] == 'tank').dropna(how='all')
+syn_under = df_all.where(df_all['SM_type'] == 'synfuel').where(df_all['sub_type'] == 'underground').dropna(how='all')
+press_cav = df_all.where(df_all['SM_type'] == 'pressure_cavern').dropna(how='all')
+phes = df_all.where(df_all['sub_type'] == 'pumped_hydro').dropna(how='all')
 
-def inv_DDmax(DD_max):
-    return np.log10(2190/DD_max)
+SM_vol = pd.concat([syn_tanks,syn_under,press_cav,phes])
 
-df_all['DDmax'] = calc_DDmax(df_all['C_kwh_log'])
+SM_vol
+#%%
+
+
+df_mat = df_all.drop(SM_vol.index)
+
+
+#%%
+# def calc_DDmax(log_C_kWh):
+#     return 2190/(10**log_C_kWh)
+
+# def inv_DDmax(DD_max):
+#     return np.log10(2190/DD_max)
+
+# df_all['DDmax'] = calc_DDmax(df_all['C_kwh_log'])
 
 
 
 
 #%%
 
-df_plot = df_all
+vol_plot_width = 1
+
+df_plot = df_mat
 
 np.random.seed(49)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(4.6 - vol_plot_width, 3))
 
 cat_label = 'display_text'
 ax = sns.stripplot(
@@ -95,27 +114,63 @@ fig.axes[0].yaxis.set_ticks([np.log10(x) for p in log_ticks for x in np.linspace
 plt.xticks(rotation=70)
 
 
-plt.ylabel('$C_{kWh,mat}$ (USD/kWh)')
+plt.ylabel('$C_{kWh,min}$ (USD/kWh)')
 plt.xlabel('Technology')
 plt.suptitle("{} Storage Media with Price and Energy data".format(len(df_plot)))
 
 
-# strip_plot(df_all)
-
-# plt.text(10,0.3, '$\phi = 100 hours$')
-
-
-# plt.gca().get_legend().set_bbox_to_anchor([0,0,1.35,1])
 plt.gca().get_legend().remove()
 plt.suptitle('')
 
-
-
 plt.tight_layout()
-plt.savefig(pjoin(output_dir,'Ckwh.png'))
+plt.savefig(pjoin(output_dir,'Ckwh_mat.png'))
 
 # %%
 
 
 
 
+
+df_plot = SM_vol
+
+np.random.seed(49)
+
+fig = plt.figure(figsize=(vol_plot_width, 3))
+
+cat_label = 'display_text'
+ax = sns.stripplot(
+    data=df_plot, 
+    x=cat_label, 
+    y='C_kwh_log', 
+    size=3, 
+    hue='energy_type', 
+    palette=palette, 
+    style='coupled', 
+    markers={'Coupled': 'X', 'Decoupled':'o'},
+    jitter=0.2,
+                )
+
+plt.axhline(np.log10(1), linestyle='--', color='gray')
+plt.axhline(np.log10(10), linestyle='-.', color='gray')
+plt.axhline(np.log10(100), linestyle=':', color='gray')
+
+fig.axes[0].yaxis.set_major_formatter(mticker.StrMethodFormatter("$10^{{{x:.0f}}}$"))
+# log_ticks = range(int(np.floor(df_plot['C_kwh_log'].min())), int(np.ceil(df_plot['C_kwh_log'].max())))
+
+fig.axes[0].yaxis.set_ticks([np.log10(x) for p in log_ticks for x in np.linspace(10**p, 10**(p+1), 10)], minor=True)
+plt.xticks(rotation=70)
+
+
+plt.ylabel('$C_{kWh,min}$ (USD/kWh)')
+plt.xlabel('Technology')
+plt.suptitle("{} Storage Media with Price and Energy data".format(len(df_plot)))
+
+
+plt.gca().get_legend().remove()
+plt.suptitle('')
+
+plt.ylabel('')
+plt.yticks([])
+
+plt.tight_layout()
+plt.savefig(pjoin(output_dir,'Ckwh_vol.png'))
