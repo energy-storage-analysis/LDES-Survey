2afdf3ea5c3a30d6763eef90afb10cd631b326e9
Lee Aspitarte <aspitarte@gmail.com>
2023-06-04 16:11:03 -0700

    clean up consolidate_data script

--- cap_cost/consolidate_data.py
+++ cap_cost/consolidate_data.py
@@ -6,20 +6,27 @@ import pandas as pd
 from es_utils import join_col_vals
 from es_utils.units import prep_df_pint_out, read_pint_df, ureg
 
+from dotenv import load_dotenv
+load_dotenv()
+REPO_DIR = os.getenv('REPO_DIR')
+
 dataset_folder = 'datasets'
 dataset_index = pd.read_csv(pjoin(dataset_folder,'dataset_index.csv'), index_col=0)
 dataset_years = pd.read_csv(pjoin(dataset_folder,'dataset_years.csv'), index_col=0)['year'].dropna()
 
+from es_utils.cpi import get_cpi_data
+cpi_data = get_cpi_data()
+
 #%%
 
-# col_select = ['material_name', 'molecular_formula', 'original_name','specific_price','specific_energy','energy_type','source']
+# Consolidate together all data from folders listed in dataset_index.csv
+
 dfs_mat_data = []
 dfs_SM = []
 
 for source, row in dataset_index.iterrows():
     fp_prices = os.path.join(dataset_folder, row['folder'], 'output', 'mat_data.csv')
     if os.path.exists(fp_prices):
-        # df_mat_data = pd.read_csv(fp_prices, header = [0,1], index_col=0)
         df_mat_data = read_pint_df(fp_prices)
         
 
@@ -38,7 +45,6 @@ for source, row in dataset_index.iterrows():
 
     fp_SM = os.path.join(dataset_folder, row['folder'], 'output', 'SM_data.csv')
     if os.path.exists(fp_SM):
-        # df_SM = pd.read_csv(fp_SM,index_col=0)
         df_SM = read_pint_df(fp_SM)
 
         #Custom data dataset already has source column
@@ -51,16 +57,22 @@ df_mat_data = pd.concat(dfs_mat_data)
 df_SM = pd.concat(dfs_SM)
 df_SM.index.name = 'SM_name'
 
-
 #We are going to index by both SM_name and SM_type, then average all duplicate values that are floats
-
 df_SM = df_SM.reset_index().set_index(['SM_name','SM_type'])
+
+#%%
+
+#Correct for inflation here
+df_mat_data['specific_price'] = df_mat_data['specific_price']*df_mat_data['year'].map(cpi_data)
+
+df_mat_data_all_out = prep_df_pint_out(df_mat_data)
+df_mat_data_all_out.to_csv('data_consolidated/mat_data_all.csv')
+
+# physprop_lookup = pd.read_csv(os.path.join(REPO_DIR, 'cap_cost\source_meta\physical_property_lookup.csv'), index_col=0)['long_name']
 
 df_SM_all_out = prep_df_pint_out(df_SM)
 df_SM_all_out.to_csv('data_consolidated/SM_data_all.csv')
 
-# Remove some columns with extra information in the final dataset. 
-df_SM = df_SM.drop(['kth', 'C_kwh_orig'], axis=1) # Drop columns that are unused in in the 
 
 #%%
 
@@ -71,6 +83,9 @@ df_SM = df_SM.drop(['kth', 'C_kwh_orig'], axis=1) # Drop columns that are unused
 # If units are not setup properly in raw dataset then the dtype is an object and an error is thrown. 
 # You can find which entries are floats (and not pint Quantity) with e.g. df_SM[df_SM['mass_density'].apply(type) == float]
 
+# Remove some columns with extra information in the final dataset. 
+df_SM = df_SM.drop(['kth', 'C_kwh_orig'], axis=1) # Drop columns that are unused in in the 
+
 float_cols = []
 for column in df_SM.columns:
     dtype = df_SM[column].dtype
@@ -79,7 +94,6 @@ for column in df_SM.columns:
 
 
 other_cols = [c for c in df_SM.columns if c not in float_cols]
-
 
 #%%
 
@@ -203,21 +217,7 @@ df_grouped_out.to_csv('data_consolidated/SM_data.csv')
 
 #%%
 
-from es_utils.cpi import get_cpi_data
-cpi_data = get_cpi_data()
-
-df_mat_data['specific_price'] = df_mat_data['specific_price']*df_mat_data['year'].map(cpi_data)
-
-#%%
-
-df_mat_data_all_out = prep_df_pint_out(df_mat_data)
-
-df_mat_data_all_out.to_csv('data_consolidated/mat_data_all.csv')
-
-## Collect prices 
-
-#%%
-
+# Combine mat data into one. 
 
 s_temp = df_mat_data.groupby('index')['source'].apply(join_col_vals)
 s_temp.name = 'sources'
@@ -310,22 +310,3 @@ df_prices_combine = df_prices_combine[[
 df_prices_combine = prep_df_pint_out(df_prices_combine)
 
 df_prices_combine.to_csv('data_consolidated/mat_data.csv')
-
-
-
-#%%
-
-
-# s_temp = df_mat_data.groupby('index')['source'].apply(join_col_vals)
-# s_temp.name = 'source'
-# df_physprop_combine = s_temp.to_frame()
-
-# df_physprop_combine['num_source'] = df_physprop_combine['source'].str.split(',').apply(len)
-
-
-# physprop_cols = ['Cp','kth','sp_latent_heat','phase_change_T','deltaH_thermochem','specific_strength', 'deltaG_chem','mass_density','dielectric_breakdown','dielectric_constant']
-
-# for col in physprop_cols:
-#     df_physprop_combine[col] = df_mat_data.groupby('index')[col].mean()
-
-# df_physprop_combine.to_csv('data_consolidated/mat_physprop.csv')
