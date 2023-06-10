eebb3c070c0590c4125b6103845cb71ad05a4364
Lee Aspitarte <aspitarte@gmail.com>
2023-06-10 02:18:41 -0700

    improve genvis shell output

--- cap_cost/figure_panels/genvis_all.sh
+++ cap_cost/figure_panels/genvis_all.sh
@@ -5,7 +5,7 @@ echo "Generating Figure components"
 for folder in thermal electrochem Ckwh_strip eda virial rhoE_matprice
 do
     cd $cwd
-    echo $folder
+    echo "---- $folder ----"
     cd $folder
     ./genvis.sh
 done
